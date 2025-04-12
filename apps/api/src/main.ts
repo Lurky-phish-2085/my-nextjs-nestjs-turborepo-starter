d5f66e153ab8990c1ae4b7c8b4718bd9665c7265
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2025-04-12 16:15:51 +0800

    Add global `ValidationPipe` and `ClassSerializerInterceptor`

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   app.use(cookieParser());
 
