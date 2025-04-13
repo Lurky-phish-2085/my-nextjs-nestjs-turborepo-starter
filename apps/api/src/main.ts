35e562cb424de17836d51db28bba7ba9a04c0a80
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2025-04-13 15:44:24 +0800

    Enable custom class validators NestJS DI integration

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,12 +2,15 @@ import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
   app.use(cookieParser());
 
