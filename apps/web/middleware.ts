7dc7ee67973e1a25aece8586b324b0adeb47786b
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2025-04-24 07:56:52 +0800

    Implement refresh access token logic in middleware

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,6 +1,6 @@
 import { AuthApi } from '@repo/api-lib/api';
 import { ApiClient } from '@repo/commons/api-client';
-import { HttpStatusCode } from 'axios';
+import { AxiosHeaders, HttpStatusCode } from 'axios';
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 import { Routes } from './constants/routes';
@@ -8,19 +8,54 @@ import { Routes } from './constants/routes';
 const api = ApiClient.use(AuthApi);
 
 export async function middleware(request: NextRequest) {
-  const authToken = request.cookies.get('Authentication');
-  const refreshToken = request.cookies.get('Refresh');
+  const { cookies } = request;
+  const accessToken = cookies.get('Authentication');
+  const refreshToken = cookies.get('Refresh');
 
-  const response = await api
+  let authResponse = await api
     .authControllerAuthenticate({
       headers: {
-        Authorization: `Bearer ${authToken?.value}`,
-        Refresh: refreshToken?.value,
+        Authorization: `Bearer ${accessToken?.value}`,
       },
     })
-    .catch((data) => {
-      return data;
-    });
+    .catch((data) => data);
+
+  let responseHeaders = new Headers();
+
+  if (authResponse.status !== HttpStatusCode.Ok) {
+    const newAccessToken = await api
+      .authControllerRefresh({
+        headers: {
+          Cookie: `Refresh=${refreshToken?.value};`,
+        },
+      })
+      .then((data) => {
+        const { headers } = data;
+
+        let setCookie = null;
+
+        if (headers instanceof AxiosHeaders) {
+          setCookie = headers.get('Set-Cookie');
+        }
+
+        if (setCookie) {
+          responseHeaders.set('Set-Cookie', setCookie.toString());
+        }
+
+        return data.data.accessToken;
+      })
+      .catch((_) => null);
+
+    if (newAccessToken) {
+      authResponse = await api
+        .authControllerAuthenticate({
+          headers: {
+            Authorization: `Bearer ${newAccessToken}`,
+          },
+        })
+        .catch((data) => data);
+    }
+  }
 
   const authExcludedRoutes = [
     Routes.AUTH_LOGIN.toString(),
@@ -29,19 +64,21 @@ export async function middleware(request: NextRequest) {
 
   if (
     authExcludedRoutes.includes(request.nextUrl.pathname) &&
-    response.status === HttpStatusCode.Ok
+    authResponse.status === HttpStatusCode.Ok
   ) {
     return NextResponse.redirect(new URL('/dashboard', request.url));
   }
 
   if (
     request.nextUrl.pathname === '/dashboard' &&
-    response.status !== HttpStatusCode.Ok
+    authResponse.status !== HttpStatusCode.Ok
   ) {
     return NextResponse.redirect(new URL(Routes.AUTH_LOGIN, request.url));
   }
 
-  return NextResponse.next();
+  return NextResponse.next({
+    headers: responseHeaders,
+  });
 }
 
 export const config = {
